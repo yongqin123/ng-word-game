e0f56a27a04dd6fd0aa4dcd75c0206a70f9056aa
yongqin123 <yongqin12324@gmail.com>
2024-06-27 17:44:38 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  template: `<h2>test</h2>`,
+  //templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class HomeComponent {
+  
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
