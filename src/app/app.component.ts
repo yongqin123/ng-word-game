9910a62a0b217c0788767c465a5788a0d7f4ef9d
yongqin123 <yongqin12324@gmail.com>
2024-06-28 11:57:44 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  template: '<h1>Hello world</h1>',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
